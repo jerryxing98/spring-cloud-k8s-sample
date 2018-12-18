165256328d2384d639eda4aa26edbff6540d0c87
JerryXing <jerryxing98@gmail.com>
2018-12-18 15:31:26 +0800

    Update SampleServiceApplication.java
    commitadd bugfixed

--- sample-service-eureka-client/src/main/java/com/alicloud/microservices/refapp/sample/SampleServiceApplication.java
+++ sample-service-eureka-client/src/main/java/com/alicloud/microservices/refapp/sample/SampleServiceApplication.java
@@ -6,10 +6,10 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.ServiceInstance;
-import org.springframework.cloud.client.discovery.DiscoveryClient;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+//import org.springframework.cloud.client.ServiceInstance;
+//import org.springframework.cloud.client.discovery.DiscoveryClient;
+//import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+//import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,8 +17,8 @@ import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 //@EnableDiscoveryClient
-//@RestController
-@EnableHystrix
+@RestController
+//@EnableHystrix
 public class SampleServiceApplication {
 
 	@Autowired
